58952f927048746099dde9f9836409d3fb487d11
Enrique Izel <eleaoizel@gmail.com>
2024-06-24 15:25:16 -0400

    feat: add the recyclerView configuration

--- app/src/main/java/com/example/fragmentsexample/fragments/ListFragment.kt
+++ app/src/main/java/com/example/fragmentsexample/fragments/ListFragment.kt
@@ -6,15 +6,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.fragmentsexample.databinding.FragmentListBinding
+import com.example.fragmentsexample.fragments.adapter.CarAdapter
+import com.example.fragmentsexample.util.CarUtils
 
 class ListFragment : Fragment() {
+
+  private lateinit var binding: FragmentListBinding
+  private val recyclerView by lazy { binding.recyclerViewList }
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
-    val binding = FragmentListBinding.inflate(inflater, container, false)
+    binding = FragmentListBinding.inflate(inflater, container, false)
     return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    val carList = CarUtils.getCars()
+    val carAdapter = CarAdapter(carList)
+
+    recyclerView.adapter = carAdapter
   }
 
   companion object {
