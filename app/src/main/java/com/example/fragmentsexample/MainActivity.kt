462893f288ff2047876c7974e083472292c10615
Enrique Izel <eleaoizel@gmail.com>
2024-06-26 16:24:07 -0400

    feat: add function to transact the fragment

--- app/src/main/java/com/example/fragmentsexample/MainActivity.kt
+++ app/src/main/java/com/example/fragmentsexample/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.fragmentsexample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.fragmentsexample.fragments.DetailsFragment
 import com.example.fragmentsexample.fragments.ListFragment
+import com.example.fragmentsexample.model.Car
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,5 +21,15 @@ class MainActivity : AppCompatActivity() {
       R.id.fragment_container_main,
       fragmentList
     ).commit()
+  }
+
+  fun openDetailFragment(car: Car) {
+    val detailsFragment = DetailsFragment.newInstance(car)
+    val transaction = supportFragmentManager.beginTransaction()
+
+    transaction.replace(R.id.fragment_container_main, detailsFragment)
+    transaction.addToBackStack(null)
+
+    transaction.commit()
   }
 }
