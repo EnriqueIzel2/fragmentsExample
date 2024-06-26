150e5715399d8f3b532c6f40b66d9b8da1354cf1
Enrique Izel <eleaoizel@gmail.com>
2024-06-26 16:23:09 -0400

    feat: add Parcelable

--- app/src/main/java/com/example/fragmentsexample/model/Car.kt
+++ app/src/main/java/com/example/fragmentsexample/model/Car.kt
@@ -1,7 +1,11 @@
 package com.example.fragmentsexample.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Car(
   val name: String,
   val brand: String,
   val options: List<String>
-)
+) : Parcelable
